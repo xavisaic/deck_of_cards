5cd7f1d2c2e01d7a02ff3a890d13f6873a302c61
Xavier Castro Rozas <x.castrorozas@gmail.com>
2022-11-07 22:40:45 -0300

    se hizo correccion del profe

--- game.py
+++ game.py
@@ -3,41 +3,42 @@ from classes.deck import Deck
 bicycle = Deck()
 
 bicycle.show_cards()
-victoria_CPU = 0
-victorias_Usuario = 0
-while True:
-    print("estamos jugando a la carta mayor\n")
 
-    mazo = Deck()
-    cartaCPU = mazo.dame_carta()
-    carta_usuario = mazo.dame_carta()
 
-    print("la carta de la CPU es")
-    cartaCPU.card_info()
+def croupier():
+    victoria_CPU = 0
+    victorias_Usuario = 0
+    while True:
 
-    print("la carta del usuario es")
-    carta_usuario.card_info()
+        print("estamos jugando a la carta mayor\n")
 
-    if cartaCPU.comparar(carta_usuario):
-        print('la casa gana')
-        victoria_CPU += 1
-    else:
-        victorias_Usuario += 1
-        print('gana el usuario')
+        mazo = Deck()
+        cartaCPU = mazo.dame_carta()
+        carta_usuario = mazo.dame_carta()
 
-    respuesta = input('''desea continuar otro juego?:
-    *presione 's' para continuar
-    *presione 'n' para salir\n''')
+        print("la carta de la CPU es")
+        cartaCPU.card_info()
 
-    if respuesta == 'n':
-        print("hasta la proxima")
-        break
-    elif respuesta == 's':
-        print("A Jugar!")
-    else:
-        print("respuesta invalida")
+        print("la carta del usuario es")
+        carta_usuario.card_info()
+
+        if cartaCPU.comparar(carta_usuario):
+            print('la casa gana')
+            victoria_CPU += 1
+        else:
+            victorias_Usuario += 1
+            print('gana el usuario')
+
+        respuesta = input('''desea continuar otro juego?:
+        *presione 's' para continuar
+        *presione 'n' para salir\n''')
+
+        if respuesta != 's':
+            print("hasta la proxima")
+            break
         
 
+croupier()
 
 
 
